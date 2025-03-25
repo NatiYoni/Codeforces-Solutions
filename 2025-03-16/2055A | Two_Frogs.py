9033a6523322a0ead0fab11b30343afc81f06669
Natnael Yonas <148974950+NatiYoni@users.noreply.github.com>
2025-03-25 09:40:09 +0300

    Add/update 2025-03-16/2055A | Two_Frogs.py

---
+++ 2025-03-16/2055A | Two_Frogs.py
@@ -0,0 +1,33 @@
+import sys, collections, math
+
+input = lambda: sys.stdin.readline().strip()
+
+inp = lambda: int(input())
+inlt = lambda: list(map(int, input().split()))
+insr = lambda: list(input())
+invr = lambda: map(int, input().split())
+
+def dd(type_func=int):
+    return collections.defaultdict(type_func)
+
+def is_in_bounds(grid, i, j):
+    return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+
+def solve():
+
+    n, a, b = invr()
+    if n > 2 and (a - b) % 2 == 0:
+        print("YES") 
+        
+    else:
+        print("NO")
+    pass
+
+def main():
+    multi = True  # Set to True for multi-test cases
+    t = inp() if multi else 1
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
